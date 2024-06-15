7bb3cde36e63ea3bb1f9bfce6ea70cfc3781ac4e
sakshitesting <sakshishrivastav2208@gmail.com>
2024-06-15 17:20:53 +0530

    added some tasks in practice-2

--- letsLearnJS/practice-2.js
+++ letsLearnJS/practice-2.js
@@ -47,3 +47,86 @@ console.log(checkEmail3("sakshishr@gmail.com")); // true
 console.log(checkEmail3("invalid-email")); // false
 
 // test is the method to compare string with regex
+
+//count vowels and consonants
+function countVowelsAndConsonants(str) {
+  let vow = 0;
+  let con = 0;
+  let obj = { vowels: 0, consonants: 0 };
+  arr = str.split("");
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      arr[i] == "a" ||
+      arr[i] == "e" ||
+      arr[i] == "i" ||
+      arr[i] == "o" ||
+      arr[i] == "u"
+    ) {
+      vow++;
+    } else if (arr[i] >= "a" && arr[i] <= "z") {
+      con++;
+    }
+  }
+  obj.vowels = vow;
+  obj.consonants = con;
+  return obj;
+}
+console.log(countVowelsAndConsonants("hello world@#$ %^123456")); // { vowels: 3, consonants: 7 }
+
+// find the longest word of the string
+function findLongestWord(str) {
+  let longestWordLen = 0;
+  let longestWord = "";
+  arr = str.split(" ");
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length >= longestWordLen) {
+      longestWordLen = arr[i].length;
+      longestWord = arr[i];
+    }
+  }
+  return `${longestWord}=${longestWordLen}`;
+}
+console.log(findLongestWord("The quick brown fox jumps over the lazy dog")); // "jumps"
+
+//write a string in title case
+function titleCase(str) {
+  let newArr = [];
+  arr = str.split(" ");
+
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i][0].toUpperCase() + arr[i].slice(1).toLowerCase());
+  }
+
+  return newArr.join(" ");
+}
+console.log(titleCase("I'm a little teA pot")); // "I'm A Little Tea Pot"
+
+//replace a replaceSubstrings
+function replaceSubstrings(str, substr, replcWord) {
+  let result;
+  arr = str.split(" ");
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].includes(substr)) {
+      arr[i] = arr[i].replace(substr, replcWord);
+    }
+  }
+  return arr.join(" ");
+}
+console.log(
+  replaceSubstrings("The rain in Spain stays mainly in the plain", "ain", "123")
+);
+// "The r123 in Sp123 stays m123ly in the pl123"
+
+//check areAnagrams
+function areAnagrams(str1, str2) {
+  let sortedStr1;
+  let sortedStr2;
+  if (str1.length === str2.length) {
+    sortedStr1 = str1.split("").sort().join();
+    sortedStr2 = str2.split("").sort().join();
+  }
+  return sortedStr1 === sortedStr2;
+}
+console.log(areAnagrams("listen", "silent")); // true
+console.log(areAnagrams("hello", "world")); // false
